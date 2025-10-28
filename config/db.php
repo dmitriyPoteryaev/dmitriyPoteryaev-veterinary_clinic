9e4bdf29bd61ddf775aeb54d0bd68510027bb4f3
dmitriyPoteryaev <dim.poteryaeff@yandex.ru>
2025-10-28 12:53:12 +0000

    fix/ change db config

--- config/db.php
+++ config/db.php
@@ -1,11 +1,12 @@
 <?php
 return [
-  'class' => yii\db\Connection::class,
-  'dsn' => 'mysql:host=mysql;dbname=app;charset=utf8mb4', // <— имя контейнера MySQL и имя БД
-  'username' => 'app',
-  'password' => 'app',
-  'charset'  => 'utf8mb4',
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=my-mysql;dbname=app;charset=utf8mb4',
+    'username' => 'app',
+    'password' => 'app',
+    'charset' => 'utf8mb4',
     'attributes' => [
-    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci'"
-  ],
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci'"
+    ],
 ];
+
